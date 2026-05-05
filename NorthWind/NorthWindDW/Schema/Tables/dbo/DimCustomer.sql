1c4665dfae7c6cfa24c0f3040cd4c64637415af4
Mauricio Erick Molina Beltran <maumbeltran@gmail.com>
2026-05-05 15:04:32 -0400

    DimCustomer table created in data warehouse

---
+++ NorthWind/NorthWindDW/Schema/Tables/dbo/DimCustomer.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[DimCustomer]
+(
+    [CustomerSK] INT IDENTITY(1,1) NOT NULL,
+    [CustomerID] NCHAR(5) NOT NULL,
+    [CompanyName] NVARCHAR(40) NOT NULL,
+    [City] NVARCHAR(15) NULL,
+    [Region] NVARCHAR(15) NULL,
+    [Country] NVARCHAR(15) NULL
+);
+GO
+
+ALTER TABLE [dbo].[DimCustomer]
+ADD CONSTRAINT PK_DimCustomer PRIMARY KEY (CustomerSK);
+GO
