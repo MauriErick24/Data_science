cbeef51af8fb36f9f96d0a7de34ec31e7cc88c9e
Arieleandrovv <Arieleandrovv@outlook.com>
2026-05-15 01:02:47 -0400

    Add changes of orderDateKey to get orders changes procedure

--- NorthWind/NorthWindOLTP/Schema/Programmability/Stored Procedures/GetOrdersChangesByRowVersion.sql
+++ NorthWind/NorthWindOLTP/Schema/Programmability/Stored Procedures/GetOrdersChangesByRowVersion.sql
@@ -8,22 +8,55 @@ BEGIN
 
     SELECT
         o.OrderID,
+
+        -- Date Keys calculadas
+        OrderDateKey =
+            CONVERT(INT, CONVERT(VARCHAR(8), o.OrderDate, 112)),
+
+        RequiredDateKey =
+            CONVERT(INT, CONVERT(VARCHAR(8), o.RequiredDate, 112)),
+
+        ShippedDateKey =
+            CASE
+                WHEN o.ShippedDate IS NULL THEN 0
+                ELSE CONVERT(INT, CONVERT(VARCHAR(8), o.ShippedDate, 112))
+            END,
+
+        -- Claves naturales para lookups
         o.CustomerID,
         o.EmployeeID,
-        o.OrderDate,
+        o.ShipVia,
+
+        -- Producto
         od.ProductID,
+
+        -- Métricas
         od.Quantity,
         od.UnitPrice,
-        od.Discount
+        od.Discount,
+
+        SalesAmount =
+            od.Quantity * od.UnitPrice * (1 - od.Discount),
+
+        -- Fechas originales
+        CAST(o.OrderDate AS DATE) AS OrderDate,
+        CAST(o.RequiredDate AS DATE) AS RequiredDate,
+        CAST(o.ShippedDate AS DATE) AS ShippedDate
+
     FROM dbo.Orders o
-    INNER JOIN dbo.OrderDetails od
+    INNER JOIN dbo.[Order Details] od
         ON o.OrderID = od.OrderID
+
     WHERE
-        (o.rowversion > CONVERT(ROWVERSION, @startRow)
-         AND o.rowversion <= CONVERT(ROWVERSION, @endRow))
-    OR
-        (od.rowversion > CONVERT(ROWVERSION, @startRow)
-         AND od.rowversion <= CONVERT(ROWVERSION, @endRow));
+        (
+            CAST(o.rowversion AS BIGINT) > @startRow
+            AND CAST(o.rowversion AS BIGINT) <= @endRow
+        )
+        OR
+        (
+            CAST(od.rowversion AS BIGINT) > @startRow
+            AND CAST(od.rowversion AS BIGINT) <= @endRow
+        );
 
 END
 GO
